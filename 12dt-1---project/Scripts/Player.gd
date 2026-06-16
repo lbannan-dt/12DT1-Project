aed92fa8d0f0d591bcec9771d39783b9384715ef
lbannan-dt <lbannan@students.mags.school.nz>
2026-06-16 15:08:23 +1200

    Add player HP

--- 12dt-1---project/Scripts/Player.gd
+++ 12dt-1---project/Scripts/Player.gd
@@ -2,7 +2,17 @@ extends CharacterBody2D
 
 const speed: int = 450
 
+var health: int = 10
+var max_hp: int = 10
 var direction: Vector2 = Vector2(0.0, 0.0)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and health < max_hp:
+		health += 1
+		print(health)
+	if event.is_action_pressed("ui_cancel"):
+		health -= 1
+		print(health)
 
 func _process(delta: float) -> void:
 	direction.x = Input.get_axis("ui_left", "ui_right")
@@ -10,8 +20,10 @@ func _process(delta: float) -> void:
 	
 	velocity = direction.normalized() * speed
 	
+	if health == 0:
+		get_tree().reload_current_scene()
+	
 	move_and_slide()
-	print(global_position)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://Levels/Battle.tscn")
